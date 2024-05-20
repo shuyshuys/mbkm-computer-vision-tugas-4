7d35e1ede227a39c10f672d33e7bd18ed9a7533d
shuyshuys <ivlyns@protonmail.com>
2024-05-20 16:06:05 +0700

    add git pull

--- run.sh
+++ run.sh
@@ -12,5 +12,8 @@ source env/bin/activate
 # Install the required packages
 pip install -r requirements.txt
 
+# Pull the latest changes from the repository
+git pull
+
 # Run the application
 python3 app.py
