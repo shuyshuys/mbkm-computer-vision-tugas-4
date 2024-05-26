517330b836cbe905aa49439cf2261675980ca35e
shuyshuys <ivlyns@protonmail.com>
2024-05-26 23:21:52 +0700

    add cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 import xmltodict
 import json
@@ -24,6 +25,7 @@ def convert_xml_to_json(url):
     return json_data
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
